b81dd784c592467bef6da17c7efbf34302b20ab7
Akash Vinodrai <P2587653@my365.dmu.ac.uk>
2022-02-22 15:14:48 +0000

    I have created the properties for my table

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,17 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int OrderNumber { get; set; }
+        public int TrackingNumber { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public string CustomerName { get; set; }
+        public bool ConfirmOrder { get; set; }
+        public DateTime OrderDate { get; set; }
     }
 }
