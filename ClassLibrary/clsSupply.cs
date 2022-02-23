4f6d1b76b4507af0fc93fff791b38952be77c805
Ameer Hussain <P2593121@my365.dmu.ac.uk>
2022-02-23 17:38:00 +0000

    Added tests for supply table attributes.

--- ClassLibrary/clsSupply.cs
+++ ClassLibrary/clsSupply.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupply
     {
+        public bool IsAvailable { get; set; }
+        public DateTime DateAvailable { get; set; }
+        public string SupplierName { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
     }
 }
--- Testing3/tstSupply.cs
+++ Testing3/tstSupply.cs
@@ -15,5 +15,70 @@ namespace Testing3
             //Test to see that it exists.
             Assert.IsNotNull(Supplier);
         }
+
+        [TestMethod]
+        public void IsAvaiablePropertyOK()
+        {
+            //Create an instance of the class we want to create.
+            clsSupply Supplier = new clsSupply();
+            //Create test data.
+            Boolean testData = true;
+            //Assign the data to the property.
+            Supplier.IsAvailable = testData;
+            //Test that the values are the same.
+            Assert.AreEqual(Supplier.IsAvailable, testData);
+        }
+
+        [TestMethod]
+        public void DateAvailablePropertyOK()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create test data.
+            DateTime testData = DateTime.Now.Date;
+            //Assign the data to the property.
+            Supplier.DateAvailable = testData;
+            //Test that the values are the same.
+            Assert.AreEqual(Supplier.DateAvailable, testData);
+        }
+
+        [TestMethod]
+        public void SupplierNamePropertyOK()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create test data.
+            String testData = "Microsoft";
+            //Assign the data to the property.
+            Supplier.SupplierName = testData;
+            //Test that the values are the same.
+            Assert.AreEqual(Supplier.SupplierName, testData);
+        }
+
+        [TestMethod]
+        public void ProductNamePropertyOK()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create test data.
+            String testData = "MacBook Pro";
+            //Assign the data to the property.
+            Supplier.ProductName = testData;
+            //Test that the values are the same.
+            Assert.AreEqual(Supplier.ProductName, testData);
+        }
+
+        [TestMethod]
+        public void ProductPricePropertyOK()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create test data.
+            double testData = 100;
+            //Assign the data to the property.
+            Supplier.ProductPrice = testData;
+            //Test that the values are the same.
+            Assert.AreEqual(Supplier.ProductPrice, testData);
+        }
     }
 }
