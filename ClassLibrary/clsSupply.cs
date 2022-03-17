9dcd44283454f71addd62793c9e01e2dd0b14ab4
Ameer Hussain <P2593121@my365.dmu.ac.uk>
2022-03-17 11:19:52 +0000

    Added find method and testing for supply.

--- ClassLibrary/clsSupply.cs
+++ ClassLibrary/clsSupply.cs
@@ -4,10 +4,82 @@ namespace ClassLibrary
 {
     public class clsSupply
     {
-        public bool IsAvailable { get; set; }
-        public DateTime DateAvailable { get; set; }
-        public string SupplierName { get; set; }
-        public string ProductName { get; set; }
-        public double ProductPrice { get; set; }
+        private bool mIsAvailable;
+        public bool IsAvailable { 
+            get
+            {
+                return mIsAvailable;
+            }
+            set
+            {
+                mIsAvailable = value;
+            }
+        }
+        private DateTime mDateAvailable;
+        public DateTime DateAvailable { 
+            get
+            {
+                return mDateAvailable;
+            }
+            set
+            {
+                mDateAvailable = value;
+            }
+        }
+        private string mSupplierName;
+        public string SupplierName { 
+            get
+            {
+                return mSupplierName;
+            }
+            set
+            {
+                mSupplierName = value;
+            }
+        }
+        private string mProductName;
+        public string ProductName { 
+            get
+            {
+                return mProductName;
+            }
+            set
+            {
+                mProductName = value;
+            }
+        }
+        private double mProductPrice;
+        public double ProductPrice { 
+            get
+            {
+                return mProductPrice;
+            }
+            set
+            {
+                mProductPrice = value;
+            } 
+        }
+        private Int32 mSupplierNo;
+        public Int32 SupplierNo { 
+            get
+            {
+                return mSupplierNo;
+            }
+            set
+            {
+                mSupplierNo = value;
+            }
+        }
+
+        public bool Find(Int32 supplierNo)
+        {
+            mSupplierNo = 2;
+            mSupplierName = "Microsoft";
+            mProductName = "Surface Laptop";
+            mProductPrice = 900;
+            mIsAvailable = true;
+            mDateAvailable = Convert.ToDateTime("10/02/2022");
+            return true;
+        }
     }
 }
--- Testing3/tstSupply.cs
+++ Testing3/tstSupply.cs
@@ -10,16 +10,29 @@ namespace Testing3
         [TestMethod]
         public void InstanceOK()
         {
-            //Create an instance of the class we want to create.
+            //Create an instance of the suplier class.
             clsSupply Supplier = new clsSupply();
             //Test to see that it exists.
             Assert.IsNotNull(Supplier);
         }
 
         [TestMethod]
+        public void SupplierNoPropertyOK()
+        {
+            //Create an instance of the supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create test data.
+            Int32 testData = 2;
+            //Assign the data to the property.
+            Supplier.SupplierNo = testData;
+            //Test that the values are the same.
+            Assert.AreEqual(Supplier.SupplierNo, testData);
+        }
+
+        [TestMethod]
         public void IsAvaiablePropertyOK()
         {
-            //Create an instance of the class we want to create.
+            //Create an instance of the suppler class.
             clsSupply Supplier = new clsSupply();
             //Create test data.
             Boolean testData = true;
@@ -79,6 +92,147 @@ namespace Testing3
             Supplier.ProductPrice = testData;
             //Test that the values are the same.
             Assert.AreEqual(Supplier.ProductPrice, testData);
+        }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //Create an instance of the supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create Boolean to store results.
+            Boolean Found = false;
+            //Create test data.
+            Int32 SupplierNo = 2;
+            //Invoke the method.
+            Found = Supplier.Find(SupplierNo);
+            //Test to see if it is true.
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestSupplierNoFound()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create Boolean to show results.
+            Boolean Found = false;
+            //Create Boolean to show data is ok.
+            Boolean OK = true;
+            //Create test data.
+            Int32 SupplierNo = 2;
+            //Invoke the method.
+            Found = Supplier.Find(SupplierNo);
+            if (Supplier.SupplierNo != 2)
+            {
+                OK = false;
+            }
+            //Test to see the result.
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestSupplierNameFound()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create Boolean to show results.
+            Boolean Found = false;
+            //Create boolean to show data is ok.
+            Boolean OK = true;
+            //Create test data.
+            Int32 SupplierNo = 2;
+            //Invoke the method.
+            Found = Supplier.Find(SupplierNo);
+            if (Supplier.SupplierName != "Microsoft")
+            {
+                OK = false;
+            }
+            //Check the results.
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestProductNameFound()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create Boolean to show results.
+            Boolean Found = false;
+            //Create boolean to show data is ok.
+            Boolean OK = true;
+            //Create test data.
+            Int32 SupplierNo = 2;
+            //Invoke the method.
+            Found = Supplier.Find(SupplierNo);
+            if (Supplier.ProductName != "Surface Laptop")
+            {
+                OK = false;
+            }
+            //Check the results.
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestProductPriceFound()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create Boolean to show results.
+            Boolean Found = false;
+            //Create boolean to show data is ok.
+            Boolean OK = true;
+            //Create test data.
+            Int32 SupplierNo = 2;
+            //Invoke the method.
+            Found = Supplier.Find(SupplierNo);
+            if (Supplier.ProductPrice != 900)
+            {
+                OK = false;
+            }
+            //Check the results.
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestIsAvailableFound()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create Boolean to show results.
+            Boolean Found = false;
+            //Create boolean to show data is ok.
+            Boolean OK = true;
+            //Create test data.
+            Int32 SupplierNo = 2;
+            //Invoke the method.
+            Found = Supplier.Find(SupplierNo);
+            if (Supplier.IsAvailable != true)
+            {
+                OK = false;
+            }
+            //Check the results.
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestDateAvailableFound()
+        {
+            //Create an instance of the Supplier class.
+            clsSupply Supplier = new clsSupply();
+            //Create Boolean to show results.
+            Boolean Found = false;
+            //Create boolean to show data is ok.
+            Boolean OK = true;
+            //Create test data.
+            Int32 SupplierNo = 2;
+            //Invoke the method.
+            Found = Supplier.Find(SupplierNo);
+            if (Supplier.DateAvailable != Convert.ToDateTime("10/02/2022"))
+            {
+                OK = false;
+            }
+            //Check the results.
+            Assert.IsTrue(OK);
         }
     }
 }
