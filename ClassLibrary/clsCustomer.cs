2a05571efcd0cc14c3caff6d35fe23df3fdd1a61
P2594573 <P2594573@my365.dmu.ac.uk>
2022-03-17 12:32:21 +0000

    Added more testing for the customer class

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool Active { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CustomerNo { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public decimal TotalPrice { get; set; }
+        public bool Over18 { get; set; }
     }
-}
+} 
