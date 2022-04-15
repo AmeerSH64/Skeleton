5fa8ab278b9cc7295cc3cdc8ab0b3372ca610f0d
Ameer Hussain <P2593121@my365.dmu.ac.uk>
2022-04-15 16:29:44 +0100

    Started SupplyCollection class and tests.

---
+++ Testing3/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstSupplyCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the Supplier collection class.
+            clsSupplyCollection AllSuppliers = new clsSupplyCollection();
+            //Test to see if it exists.
+            Assert.IsNotNull(AllSuppliers);
+        }
+
+        [TestMethod]
+        public void SupplierListOK()
+        {
+            //Create an instance of the Supplier collection class.
+            clsSupplyCollection AllSuppliers = new clsSupplyCollection();
+            //Create test data which will be a list.
+            List<clsSupply> TestList = new List<clsSupply>();
+            //Add an item to the list.
+            clsSupply TestItem = new clsSupply();
+            //Set its properties.
+            TestItem.SupplierNo = 5;
+            TestItem.SupplierName = "Apple";
+            TestItem.ProductName = "iMac";
+            TestItem.ProductPrice = 1500;
+            TestItem.DateAvailable = DateTime.Now.Date;
+            TestItem.IsAvailable = true;
+            //Add the item to the test list.
+            TestList.Add(TestItem);
+            //Assign the data to the property.
+            AllSuppliers.SupplierList = TestList;
+            //Test to see if the values match.
+            Assert.AreEqual(AllSuppliers.SupplierList, TestList);
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //Create an instance of the Supplier collection class.
+            clsSupplyCollection AllSuppliers = new clsSupplyCollection();
+            //Create test data.
+            Int32 SomeCount = 0;
+            //Assign the data to the property.
+            AllSuppliers.Count = SomeCount;
+            //Test to see if the values match.
+            Assert.AreEqual(AllSuppliers.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisSupplierPropertyOK()
+        {
+            //Create an instance of the Supplier collection class.
+            clsSupplyCollection AllSuppliers = new clsSupplyCollection();
+            //Create test data.
+            clsSupply TestSupplier = new clsSupply();
+            //Set the properrties to the object.
+            TestSupplier.SupplierNo = 5;
+            TestSupplier.SupplierName = "Apple";
+            TestSupplier.ProductName = "iMac";
+            TestSupplier.ProductPrice = 1500;
+            TestSupplier.DateAvailable = DateTime.Now.Date;
+            TestSupplier.IsAvailable = true;
+            //Assign the data to the property.
+            AllSuppliers.ThisSupplier = TestSupplier;
+            //Test to see if the values match.
+            Assert.AreEqual(AllSuppliers.ThisSupplier, TestSupplier);
+        }
+    }
+}
