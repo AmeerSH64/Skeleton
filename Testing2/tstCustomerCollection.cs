1681ffd65d6da65a48d32a091c1fb6d773930256
P2594573 <P2594573@my365.dmu.ac.uk>
2022-04-25 01:14:31 +0100

    finished week 26

--- Testing2/tstCustomerCollection.cs
+++ Testing2/tstCustomerCollection.cs
@@ -43,13 +43,62 @@ namespace Testing2
             //test to se that the two value are the same
             Assert.AreEqual(AllCustomers.CustomerList, TestList);
 
-        
+
+        }
+
+
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            //Create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //create some test data to assign to the property
+            clsCustomer TestCustomer = new clsCustomer();
+            //set the properties of the test object
+            TestCustomer.Over18 = true;
+            TestCustomer.CustomerNo = 6;
+            TestCustomer.CustomerName = "Test Customer Name";
+            TestCustomer.DateOfBirth = DateTime.Now.Date;
+            TestCustomer.Address = "Test Address";
+            TestCustomer.TotalPrice = 100;
+            //asssign the data to the property
+            AllCustomers.ThisCustomer = TestCustomer;
+            //test to that the two value are the same
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+
+
         }
 
         [TestMethod]
-        public void CounterPropertyOK()
+        public void ListAndCountOK()
         {
+            //Create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //creste some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            //add an item to the  list
+            //create the item of the test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its properties
+            TestItem.Over18 = true;
+            TestItem.CustomerNo = 6;
+            TestItem.CustomerName = "Test Customer Name";
+            TestItem.DateOfBirth = DateTime.Now.Date;
+            TestItem.Address = "Test Address";
+            TestItem.TotalPrice = 100;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllCustomers.CustomerList = TestList;
+            //test to see that the two value are the same
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
 
         }
-    }
+
+        
+    }  
+    
 }
+
+
