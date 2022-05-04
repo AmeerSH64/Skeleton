d3fc107907a67ca57b5395de584095e3a09aaa59
Ameer Hussain <P2593121@my365.dmu.ac.uk>
2022-05-04 11:01:48 +0100

    Final checks.

--- AdminSystem/SupplyConfirmDelete.aspx.cs
+++ AdminSystem/SupplyConfirmDelete.aspx.cs
@@ -8,11 +8,13 @@ using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    //Variable to store the primary key of the record being deleted.
     Int32 SupplierNo;
+    //Event handler for load.
     protected void Page_Load(object sender, EventArgs e)
     {
         //Get the record to be deleted from the session object.
-        SupplierNo = Convert.ToInt32(Session["AddressNo"]);
+        SupplierNo = Convert.ToInt32(Session["SupplierNo"]);
     }
 
     protected void btnYes_Click(object sender, EventArgs e)
